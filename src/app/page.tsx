388adcdc6b086877f6491c606080d253db5f1482
Ikhsan Muttaqin <ikhsanmutta32@gmail.com>
2025-07-21 02:49:14 +0000

    perbesar gambar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
                   <div className="absolute inset-0 bg-black/50" />
                   <div className="relative z-10 container mx-auto px-4 h-full grid grid-cols-1 md:grid-cols-2 items-center">
                     <div className="hidden md:flex justify-start items-center h-full">
-                       <div key={`image-${currentSlide}`} className="relative w-3/4 h-3/4 animate-fade-in-left">
+                       <div key={`image-${currentSlide}`} className="relative w-full h-full animate-fade-in-left">
                           <Image 
                               src={slide.productImageUrl} 
                               alt={slide.title} 
